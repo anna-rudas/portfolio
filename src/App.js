ab74daa712cddb09f9e3859386345e7c6efed8c4
Anna Rudas <rudas.annamaria@gmail.com>
2022-10-13 15:18:18 +0200

    add basic styles and css variables

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from "react";
 import { createRoot } from "react-dom/client";
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <div className="wrapper">App</div>;
 }
 
-export default App
+export default App;
 
 createRoot(document.getElementById("root")).render(<App />);
