608add169241cfa33cd1fbc48aa7b55875993727
Anna Rudas <rudas.annamaria@gmail.com>
2022-10-19 11:08:52 +0200

    add Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import MyProjects from "./components/MyProjects";
 
@@ -12,6 +13,7 @@ function App() {
       <About />
       <MyProjects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { className } from "../../helpers";
+import style from "./Footer.module.css";
+import LinkedInIcon from "../../icons/LinkedInIcon";
+import EnvelopeIcon from "../../icons/EnvelopeIcon";
+import GithubIcon from "../../icons/GithubIcon";
+
+function Footer() {
+  return (
+    <div {...className(style.footer)}>
+      <div {...className(style.iconCon)}>
+        <a href=".">
+          <LinkedInIcon {...className(style.icons)} />
+        </a>
+        <a href=".">
+          <EnvelopeIcon {...className(style.icons)} />
+        </a>
+        <a href=".">
+          {" "}
+          <GithubIcon {...className(style.icons)} />
+        </a>
+      </div>
+      <span {...className(style.credit)}>Designed and build by Anna Rudas</span>
+    </div>
+  );
+}
+
+export default Footer;
