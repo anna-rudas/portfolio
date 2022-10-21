ea8a1e7fdbba8e00ef5af82b49988cbc8e360192
Anna Rudas <rudas.annamaria@gmail.com>
2022-10-21 15:58:51 +0200

    add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 import Projects from "./Projects";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/projects" element={<Projects />} />
