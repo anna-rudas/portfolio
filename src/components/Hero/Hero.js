8055d346977b2847eb58d5e37cee4e75873397ed
Anna Rudas <rudas.annamaria@gmail.com>
2022-10-25 17:20:26 +0200

    hero height fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { className } from "../../helpers";
 import * as style from "./Hero.module.css";
 import * as shared from "../shared.module.css";
 import { Link } from "react-router-dom";
 import ArrowIcon from "../../icons/ArrowIcon";
 
+const convertStyle = () => {
+  let height = window.innerHeight;
+  document.documentElement.style.setProperty("--hero-height", `${height}px`);
+};
+
 function Hero({ before, main, intro, btnText, btnLink, isItMain, arrowLink }) {
+  useEffect(() => {
+    let width = null;
+
+    const handleResize = () => {
+      if (window.innerWidth !== width) {
+        width = window.innerWidth;
+        convertStyle();
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div {...className(style.hero)}>
       <span {...className(shared.paragraphHero)}>{before}</span>
